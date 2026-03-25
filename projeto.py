498ae976c7b740fb2059a01f173f3e2514e0deb2
Vinicios Rafael Schimuneck <viniciosrafaelschimuneck@gmail.com>
2026-03-25 11:27:07 -0300

    primeiro commit

---
+++ projeto.py
@@ -0,0 +1,26 @@
+class No:
+    def __init__(self, titulo, diretor, ano, genero):
+        self.titulo = titulo
+        self.diretor = diretor
+        self.ano = ano
+        self.genero = genero
+        self.proximo = None
+    
+    def mostrar_no(self):
+        print(f"Título: {self.titulo}")
+        print(f"Diretor: {self.diretor}")
+        print(f"Ano: {self.ano}")
+        print(f"Gênero: {self.genero}")
+
+    class CatalogoFilmes:
+        def Incluir(self, titulo, diretor, ano, genero):
+            novo_filme = No(titulo, diretor, ano, genero)
+
+            if self.primeiro is None:
+                self.primeiro = novo_filme
+            else:
+                atual = self.primeiro
+                while atual.proximo is not None:
+                    atual = atual.proximo
+                atual.proximo = novo_filme
+            print("Filme incluído com sucesso!")
