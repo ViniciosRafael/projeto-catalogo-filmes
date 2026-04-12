2f73db273ec1844af0f3b88fe8456d75874591f1
Vinicios Rafael Schimuneck <viniciosrafaelschimuneck@gmail.com>
2026-04-12 15:56:37 -0300

    ajuste de valor pra ID

--- projeto.py
+++ projeto.py
@@ -29,22 +29,22 @@ class CatalogoFilmes:
             self.tamanho = tamanho
             self.tabela = [None] * tamanho           # Inicializa a tabela com vazios.
 
-    def funcao_hash(self, valor):
-        return int(valor) % self.tamanho         # Função hash de resto inteiro que calcula o índice baseado no valor.
+    def funcao_hash(self, id):
+        return int(id) % self.tamanho         # Função hash de resto inteiro que calcula o índice baseado no id.
 
-    def inserir(self, valor, endereco_memoria):
-        indice = self.funcao_hash(valor)         # Inserir referência na tabela hash.
+    def inserir(self, id, endereco_memoria):
+        indice = self.funcao_hash(id)         # Inserir referência na tabela hash.
         if self.tabela[indice] is None:
             self.tabela[indice] = []
-        self.tabela[indice].append((valor, endereco_memoria))
+        self.tabela[indice].append((id, endereco_memoria))
 
-    def buscar(self, valor):
-        indice = self.funcao_hash(valor)         # Método para buscar um valor usando a tabela hash.
+    def buscar(self, id):
+        indice = self.funcao_hash(id)         # Método para buscar um id usando a tabela hash.
         if self.tabela[indice] is not None:
             for chave, endereco in self.tabela[indice]:
-                if chave == valor:
+                if chave == id:
                     return endereco
-        return None                              # Retorna None se o valor não for encontrado.
+        return None                              # Retorna None se o id não for encontrado.
 
     def povoar_catalogo(self, quantidade=1000):
         print(f"Povoando o catálogo com {quantidade} filmes...")
